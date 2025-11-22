93e4d5991df42f7f3f8fb40e5b801efd4d96d021
syedbadhus1 <syedbadhus1@gmail.com>
2025-11-22 19:36:41 +0400

    script

--- Variable.js
+++ Variable.js
@@ -1,6 +1,6 @@
 // Variables Declaration
 var name = 'syed';
 var age = 30;
-var nristatus = 'yes'
-var wealth = 'null'
-var currentlocation = 
+var nristatus = yes;
+var wealth = null;
+var currentlocation ;
