113364bb53d5501b8a8e1f3a9535246f7cf28445
syedbadhus1 <syedbadhus1@gmail.com>
2025-11-22 19:59:19 +0400

    updated

--- Variable.js
+++ Variable.js
@@ -1,6 +1,22 @@
-// Variables Declaration
+// Variables Declaration - 1.Strings 2.Numbers 3.Boolean 4.Null 5.Undefined
 var name = 'syed';
 var age = 30;
-var nristatus = yes;
+var nristatus = true;
 var wealth = null;
 var currentlocation ;
+
+// --> Display Varibales in Colsole (browser-Inspect-console)
+// console.log (name)
+// console.log (age)
+// console.log (nristatus)
+// console.log (wealth)
+// console.log (currentlocation)
+
+// --> To print variables in browser
+document.writeln (name);
+document.writeln (age);
+
+// --> To display "syed" in browser pop-up 
+alert (name)
+confirm ("Your name is Syed ?")
+prompt ("Enter your name: ")
